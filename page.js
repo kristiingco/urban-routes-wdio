367a0bcb04cac99c552b434994ad7e103a28035d
Kristiana Ingco <kristiana.d.ingco@gmail.com>
2024-02-23 00:17:21 -0800

    Add functionality for clicking supportive plan button

--- page.js
+++ page.js
@@ -1,37 +1,45 @@
 module.exports = {
     // Inputs
-    fromField: '#from',
-    toField: '#to',
-    phoneNumberField: '#phone',
-    codeField: '#code',
+    fromField: "#from",
+    toField: "#to",
+    phoneNumberField: "#phone",
+    codeField: "#code",
     // Buttons
-    callATaxiButton: 'button=Call a taxi',
+    callATaxiButton: "button=Call a taxi",
+    supportivePlanButton: "//div[contains(@class, 'tcard')][5]",
     phoneNumberButton: '//div[starts-with(text(), "Phone number")]',
-    nextButton: 'button=Next',
-    confirmButton: 'button=Confirm',
+    nextButton: "button=Next",
+    confirmButton: "button=Confirm",
     // Modals
-    phoneNumberModal: '.modal',
+    phoneNumberModal: ".modal",
     // Functions
-    fillAddresses: async function(from, to) {
+    fillAddresses: async function (from, to) {
         const fromField = await $(this.fromField);
         await fromField.setValue(from);
         const toField = await $(this.toField);
         await toField.setValue(to);
+    },
+    clickCallATaxiButton: async function () {
         const callATaxiButton = await $(this.callATaxiButton);
         await callATaxiButton.waitForDisplayed();
         await callATaxiButton.click();
     },
-    fillPhoneNumber: async function(phoneNumber) {
+    clickSupportivePlanButton: async function () {
+        const supportivePlanButton = await $(this.supportivePlanButton);
+        await supportivePlanButton.waitForDisplayed();
+        await supportivePlanButton.click();
+    },
+    fillPhoneNumber: async function (phoneNumber) {
         const phoneNumberButton = await $(this.phoneNumberButton);
         await phoneNumberButton.waitForDisplayed();
         await phoneNumberButton.click();
         const phoneNumberModal = await $(this.phoneNumberModal);
-        await phoneNumberModal.waitForDisplayed()
+        await phoneNumberModal.waitForDisplayed();
         const phoneNumberField = await $(this.phoneNumberField);
         await phoneNumberField.waitForDisplayed();
         await phoneNumberField.setValue(phoneNumber);
     },
-    submitPhoneNumber: async function(phoneNumber) {
+    submitPhoneNumber: async function (phoneNumber) {
         await this.fillPhoneNumber(phoneNumber);
         // we are starting interception of request from the moment of method call
         await browser.setupInterceptor();
@@ -43,9 +51,9 @@ module.exports = {
         // collect all responses
         const requests = await browser.getRequests();
         // use first response
-        await expect(requests.length).toBe(1)
-        const code = await requests[0].response.body.code
-        await codeField.setValue(code)
-        await $(this.confirmButton).click()
+        await expect(requests.length).toBe(1);
+        const code = await requests[0].response.body.code;
+        await codeField.setValue(code);
+        await $(this.confirmButton).click();
     },
 };
--- test/specs/createAnOrder.e2e.js
+++ test/specs/createAnOrder.e2e.js
@@ -1,23 +1,25 @@
-const page = require('../../page');
-const helper = require('../../helper')
+const page = require("../../page");
+const helper = require("../../helper");
 
-describe('Create an order', () => {
-    it('should open phone number modal', async () => {
-        await browser.url(`/`)
-        await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
+describe("Create an order", () => {
+    it("should select Call a Taxi with the supportive plan ", async () => {
+        await browser.url(`/`);
+        await page.fillAddresses("East 2nd Street, 601", "1300 1st St");
+        await page.clickCallATaxiButton();
+    });
+    it("should open phone number modal", async () => {
         const phoneNumberButton = await $(page.phoneNumberButton);
         await phoneNumberButton.waitForDisplayed();
         await phoneNumberButton.click();
-        const pnoneNumberModal = await $(page.phoneNumberModal);
-        await expect(pnoneNumberModal).toBeExisting();
-    })
+        const phoneNumberModal = await $(page.phoneNumberModal);
+        await expect(phoneNumberModal).toBeExisting();
+    });
 
-    it('should save the phone', async () => {
-        await browser.url(`/`)
-        await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
+    it("should save the phone", async () => {
+        await browser.url(`/`);
+        await page.fillAddresses("East 2nd Street, 601", "1300 1st St");
         const phoneNumber = helper.getPhoneNumber("+1");
         await page.submitPhoneNumber(phoneNumber);
         await expect(await helper.getElementByText(phoneNumber)).toBeExisting();
-    })
-})
-
+    });
+});
