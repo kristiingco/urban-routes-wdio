2b5132d3751fc5e6842af8c328146a3722688ff0
Kristiana Ingco <kristiana.d.ingco@gmail.com>
2024-02-23 16:49:24 -0800

    Add helper functions and test for adding card

--- helper.js
+++ helper.js
@@ -1,9 +1,15 @@
 module.exports = {
-    getPhoneNumber: function(countryCode) {
-        const number = Math.floor(1000000000 + Math.random() * 9000000000)
-        return `${countryCode}${number}`
+    getPhoneNumber: function (countryCode) {
+        const number = Math.floor(1000000000 + Math.random() * 9000000000);
+        return `${countryCode}${number}`;
     },
-    getElementByText: async function(obj) {
+    getCardNumber: function () {
+        return Math.floor(100000000000 + Math.random() * 900000000000);
+    },
+    getCVV: function () {
+        return Math.floor(10 + Math.random() * 90);
+    },
+    getElementByText: async function (obj) {
         return await $(`div=${obj.toString()}`);
-    }
+    },
 };
--- test/specs/createAnOrder.e2e.js
+++ test/specs/createAnOrder.e2e.js
@@ -2,7 +2,7 @@ const page = require("../../page");
 const helper = require("../../helper");
 
 describe("Create an order", () => {
-    it("should select Call a Taxi with the supportive plan ", async () => {
+    it("should click Call a Taxi button with the supportive plan ", async () => {
         await browser.url(`/`);
         await page.fillAddresses("East 2nd Street, 601", "1300 1st St");
         await page.clickCallATaxiButton();
@@ -23,5 +23,14 @@ describe("Create an order", () => {
         const phoneNumber = helper.getPhoneNumber("+1");
         await page.submitPhoneNumber(phoneNumber);
         await expect(await helper.getElementByText(phoneNumber)).toBeExisting();
+    });
+
+    it("should add a credit card", async () => {
+        const cardNumber = helper.getCardNumber();
+        const cvv = helper.getCVV();
+        await page.submitCardDetails(cardNumber, cvv);
+        const paymentMethodValue = await $(".pp-value .pp-value-text");
+        const paymentMethodValueText = await paymentMethodValue.getText();
+        await expect(paymentMethodValueText).toHaveText("Card");
     });
 });
