2299425d20fa79d5f28e585b82f3f5067dfc8759
Kristiana Ingco <kristiana.d.ingco@gmail.com>
2024-02-29 22:32:41 -0800

    Modify tests

--- test/specs/createAnOrder.e2e.js
+++ test/specs/createAnOrder.e2e.js
@@ -2,7 +2,20 @@ const page = require("../../page");
 const helper = require("../../helper");
 
 describe("Create an order", () => {
-    it("should click Call a Taxi button with the supportive plan ", async () => {
+    it("should set the address", async () => {
+        await browser.url(`/`);
+        await page.fillAddresses("East 2nd Street, 601", "1300 1st St");
+        const fromField = await $(page.fromField);
+        const toField = await $(page.toField);
+        await expect(fromField).toHaveValue("East 2nd Street, 601", {
+            message: "From field has incorrect input",
+        });
+        await expect(toField).toHaveValue("1300 1st St", {
+            message: "To field has incorrect input",
+        });
+    });
+
+    it("should select supportive plan ", async () => {
         await browser.url(`/`);
         await page.fillAddresses("East 2nd Street, 601", "1300 1st St");
         await page.clickCallATaxiButton();
@@ -12,12 +25,22 @@ describe("Create an order", () => {
     });
 
     it("should submit phone number", async () => {
+        await browser.url(`/`);
+        await page.fillAddresses("East 2nd Street, 601", "1300 1st St");
+        await page.clickCallATaxiButton();
+        await page.clickSupportivePlanButton();
         const phoneNumber = helper.getPhoneNumber("+1");
         await page.submitPhoneNumber(phoneNumber);
         await expect(await helper.getElementByText(phoneNumber)).toBeExisting();
     });
 
     it("should add a credit card", async () => {
+        await browser.url(`/`);
+        await page.fillAddresses("East 2nd Street, 601", "1300 1st St");
+        await page.clickCallATaxiButton();
+        await page.clickSupportivePlanButton();
+        const phoneNumber = helper.getPhoneNumber("+1");
+        await page.submitPhoneNumber(phoneNumber);
         const cardNumber = helper.getCardNumber();
         const cvv = helper.getCVV();
         await page.submitCardDetails(cardNumber, cvv);
@@ -26,6 +49,15 @@ describe("Create an order", () => {
     });
 
     it("should add message to driver", async () => {
+        await browser.url(`/`);
+        await page.fillAddresses("East 2nd Street, 601", "1300 1st St");
+        await page.clickCallATaxiButton();
+        await page.clickSupportivePlanButton();
+        const phoneNumber = helper.getPhoneNumber("+1");
+        await page.submitPhoneNumber(phoneNumber);
+        const cardNumber = helper.getCardNumber();
+        const cvv = helper.getCVV();
+        await page.submitCardDetails(cardNumber, cvv);
         const input = "Get some chips, please";
         await page.fillMessageToDriverField(input);
         const messageToDriverField = await $(page.messageToDriverField);
@@ -33,6 +65,17 @@ describe("Create an order", () => {
     });
 
     it("should select blanket and handkerchiefs", async () => {
+        await browser.url(`/`);
+        await page.fillAddresses("East 2nd Street, 601", "1300 1st St");
+        await page.clickCallATaxiButton();
+        await page.clickSupportivePlanButton();
+        const phoneNumber = helper.getPhoneNumber("+1");
+        await page.submitPhoneNumber(phoneNumber);
+        const cardNumber = helper.getCardNumber();
+        const cvv = helper.getCVV();
+        await page.submitCardDetails(cardNumber, cvv);
+        const input = "Get some chips, please";
+        await page.fillMessageToDriverField(input);
         await page.clickBlanketAndHandkerchiefsCheckbox();
         const blanketAndHandkerchiefsInput = await $(
             page.blanketAndHandkerchiefsInput
@@ -41,19 +84,61 @@ describe("Create an order", () => {
     });
 
     it("should order 2 ice creams", async () => {
+        await browser.url(`/`);
+        await page.fillAddresses("East 2nd Street, 601", "1300 1st St");
+        await page.clickCallATaxiButton();
+        await page.clickSupportivePlanButton();
+        const phoneNumber = helper.getPhoneNumber("+1");
+        await page.submitPhoneNumber(phoneNumber);
+        const cardNumber = helper.getCardNumber();
+        const cvv = helper.getCVV();
+        await page.submitCardDetails(cardNumber, cvv);
+        const input = "Get some chips, please";
+        await page.fillMessageToDriverField(input);
+        await page.clickBlanketAndHandkerchiefsCheckbox();
         await page.clickIceCreamCounterPlus();
         await page.clickIceCreamCounterPlus();
         const iceCreamCounterValue = await $(page.iceCreamCounterValue);
         await expect(iceCreamCounterValue).toHaveText("2");
     });
 
-    it("should show car search model", async () => {
+    it("should show car search modal", async () => {
+        await browser.url(`/`);
+        await page.fillAddresses("East 2nd Street, 601", "1300 1st St");
+        await page.clickCallATaxiButton();
+        await page.clickSupportivePlanButton();
+        const phoneNumber = helper.getPhoneNumber("+1");
+        await page.submitPhoneNumber(phoneNumber);
+        const cardNumber = helper.getCardNumber();
+        const cvv = helper.getCVV();
+        await page.submitCardDetails(cardNumber, cvv);
+        const input = "Get some chips, please";
+        await page.fillMessageToDriverField(input);
+        await page.clickBlanketAndHandkerchiefsCheckbox();
+        await page.clickIceCreamCounterPlus();
+        await page.clickIceCreamCounterPlus();
         await page.clickOrderButton();
         const carSearchModal = await $(page.carSearchModal);
         await expect(carSearchModal).toBeExisting();
     });
 
     it("should show driver information", async () => {
+        await browser.url(`/`);
+        await page.fillAddresses("East 2nd Street, 601", "1300 1st St");
+        await page.clickCallATaxiButton();
+        await page.clickSupportivePlanButton();
+        const phoneNumber = helper.getPhoneNumber("+1");
+        await page.submitPhoneNumber(phoneNumber);
+        const cardNumber = helper.getCardNumber();
+        const cvv = helper.getCVV();
+        await page.submitCardDetails(cardNumber, cvv);
+        const input = "Get some chips, please";
+        // if message to driver is not provided, driver information modal does not appear, which will result in the test to fail
+        await page.fillMessageToDriverField(input);
+        await page.clickBlanketAndHandkerchiefsCheckbox();
+        await page.clickIceCreamCounterPlus();
+        await page.clickIceCreamCounterPlus();
+        await page.clickOrderButton();
         const driverInformationText = await $(page.driverInformationText);
         // eslint-disable-next-line wdio/no-pause
         await browser.pause(40000);
