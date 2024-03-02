e32c8415c81e7890c8095e8bc910d43e49ca4580
Kristiana Ingco <kristiana.d.ingco@gmail.com>
2024-03-01 20:28:14 -0800

    Change timeout duration

--- wdio.conf.js
+++ wdio.conf.js
@@ -20,7 +20,7 @@ exports.config = {
     bail: 0,
     baseUrl:
         "https://cnt-b2ce6f23-3114-44f5-a955-531a9599cda6.containerhub.tripleten-services.com",
-    waitforTimeout: 10000,
+    waitforTimeout: 10000000,
     connectionRetryTimeout: 120000,
     connectionRetryCount: 3,
     services: ["geckodriver", "intercept"],
